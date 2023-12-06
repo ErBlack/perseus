5a61b546bb672727e643e36f573876d2cf888e95
Jeremy Wiebe <jeremy@khanacademy.org>
2023-12-06 13:27:14 -0800

    Enable point dragging

--- packages/perseus/src/widgets/__stories__/new-graph.stories.tsx
+++ packages/perseus/src/widgets/__stories__/new-graph.stories.tsx
@@ -1,6 +1,7 @@
 import Button from "@khanacademy/wonder-blocks-button";
 import {View} from "@khanacademy/wonder-blocks-core";
 import * as Highcharts from "highcharts";
+import draggable from "highcharts/modules/draggable-points";
 import {HighchartsReact} from "highcharts-react-official";
 import * as React from "react";
 
@@ -15,23 +16,15 @@ import type {
 
 type Story = StoryObj<typeof NewGraph>;
 
-const options: HighchartsReactProps["options"] = {
+draggable(Highcharts);
+
+const defaultOptions: HighchartsReactProps["options"] = {
     title: {
         text: undefined,
     },
     chart: {
         type: "scatter",
-        events: {
-            click: function (e) {
-                // find the clicked values and the series
-                const x = Math.round(e.xAxis[0].value);
-                const y = Math.round(e.yAxis[0].value);
-                const series = this.series[0];
-
-                // Add it
-                series.addPoint([x, y]);
-            },
-        },
+        events: {},
     },
     xAxis: {
         tickPixelInterval: 1,
@@ -40,25 +33,48 @@ const options: HighchartsReactProps["options"] = {
         maxPadding: 0.2,
         min: -10,
         max: 10,
+        plotLines: [
+            {
+                color: "#888888",
+                zIndex: 1,
+                width: 1.5,
+                dashStyle: "Solid",
+                value: 0,
+            },
+        ],
     },
     yAxis: {
         tickPixelInterval: 1,
         min: -10,
         max: 10,
+        plotLines: [
+            {
+                color: "#888888",
+                zIndex: 1,
+                width: 1.5,
+                dashStyle: "Solid",
+                value: 0,
+            },
+        ],
     },
-    series: [
-        {
-            data: [[2, 2]],
-            color: "#00cc00",
-        },
-    ],
+    series: [{type: "scatter", data: [[2, 2]], color: "#00cc00"}],
 };
 
-const NewGraph = ({size = [400, 400]}: {size: Size}) => {
-    const chart = React.useRef<HighchartsReactRefObject>();
+const NewGraph = (props: {
+    size?: Size;
+    options: HighchartsReact.Props["options"];
+}) => {
+    const chart = React.useRef<HighchartsReactRefObject>(null);
     const [chartData, setChartData] = React.useState<
         object | ReadonlyArray<any> | undefined
     >();
+    const size = React.useMemo(
+        () => ({
+            width: props.size?.[0] ?? 400,
+            height: props.size?.[1] ?? 400,
+        }),
+        [props.size],
+    );
 
     const handleDumpData = React.useCallback(() => {
         setChartData(
@@ -75,11 +91,10 @@ const NewGraph = ({size = [400, 400]}: {size: Size}) => {
                         highcharts={Highcharts}
                         ref={chart}
                         options={{
-                            ...options,
+                            ...props.options,
                             chart: {
-                                ...options?.chart,
-                                width: size[0],
-                                height: size[1],
+                                ...props.options?.chart,
+                                ...size,
                             },
                         }}
                     />
@@ -98,8 +113,77 @@ const meta: Meta<typeof NewGraph> = {
     component: NewGraph,
 };
 
-export const PointsClickToAdd: Story = {
-    args: {},
+// https://www.highcharts.com/demo/highcharts/dynamic-click-to-add
+export const PointsDemoAddRemoveDrag: Story = {
+    args: {
+        options: {
+            ...defaultOptions,
+            title: {
+                ...defaultOptions.title,
+                text: "Click to add a point, click a point to remove it, drag any point to move it",
+            },
+            chart: {
+                ...defaultOptions.chart,
+                events: {
+                    // Click to add a point
+                    click: function (e) {
+                        // find the clicked values and the series
+                        // @ts-expect-error - types are wrong
+                        const x = Math.round(e.xAxis[0].value);
+                        // @ts-expect-error - types are wrong
+                        const y = Math.round(e.yAxis[0].value);
+                        const series = this.series[0];
+
+                        // Add it
+                        series.addPoint([x, y]);
+                    },
+                },
+            },
+            series: [
+                {
+                    type: "scatter",
+                    color: "#00cc00",
+                    cursor: "grab",
+
+                    dragDrop: {
+                        draggableX: true,
+                        draggableY: true,
+                        dragPrecisionX: 1,
+                        dragPrecisionY: 1,
+                    },
+                    events: {
+                        // Click to remove a point
+                        click: function (e) {
+                            const clickedPoint = e.point;
+                            clickedPoint.remove();
+                        },
+                    },
+                    point: {
+                        events: {
+                            dragStart: function (e) {
+                                this.series.update({
+                                    type: "scatter",
+                                    cursor: "grabbing",
+                                });
+                            },
+                            drop: function (e) {
+                                this.series.update({
+                                    type: "scatter",
+                                    cursor: "grab",
+                                });
+                            },
+                        },
+                    },
+                    data: [
+                        [2, 2],
+                        [5, 5],
+                        [-3, 4],
+                        [-5, -4],
+                    ],
+                },
+            ],
+        },
+    },
 };
 
 export default meta;
