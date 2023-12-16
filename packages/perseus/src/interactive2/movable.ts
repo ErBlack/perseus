90c6076a04be150f894d15370dee41dbea7b217d
Jeremy Wiebe <jeremy@khanacademy.org>
2023-12-15 22:02:27 -0800

    Movable mostly ported to a class

--- packages/perseus/src/interactive2/movable.ts
+++ packages/perseus/src/interactive2/movable.ts
@@ -12,12 +12,22 @@
 import {point as kpoint} from "@khanacademy/kmath";
 import $ from "jquery";
 import _ from "underscore";
+import {Errors} from "../logging/log";
+import {PerseusError} from "../perseus-error";
 
 import InteractiveUtil from "./interactive-util";
 
 const normalizeOptions = InteractiveUtil.normalizeOptions;
 
 const assert = InteractiveUtil.assert;
+
+function getKey(eventName: any, id: any) {
+    return eventName + ":" + id;
+}
+
+function getEventName(key) {
+    return key.split(":")[0];
+}
 
 // state parameters that should be converted into an array of
 // functions
@@ -49,56 +59,207 @@ const DEFAULT_STATE = {
     mouseTarget: null,
 } as const;
 
+type State = {
+    constraints: any;
+    // Event Handlers
+    add: [];
+    modify: [];
+    draw: [];
+    remove: Array<(remove: any) => unknown>;
+    onMoveStart: Array<
+        (
+            onMoveStart: any,
+            startMouseCoord: any,
+            startMouseCoord1: any,
+        ) => unknown
+    >;
+    onMove: Array<
+        (onMove: any, mouseCoord: any, prevMouseCoord: any) => unknown
+    >;
+    onMoveEnd: Array<
+        (onMoveEnd: any, prevMouseCoord: any, startMouseCoord: any) => unknown
+    >;
+    onClick: Array<
+        (onClick: any, prevMouseCoord: any, startMouseCoord: any) => unknown
+    >;
+
+    // State
+    id: string;
+    mouseTarget: any;
+    added: any;
+    isMouseOver: boolean;
+    isDragging: boolean;
+    isHovering: boolean;
+};
+
 class Movable {
     graphie: any;
-    state: {
-        // Set here because this must be unique for each instance
-        id: string;
-    };
+    state: State;
+    prevState?: State;
+    private _listenerMap: any;
 
     constructor(graphie: any, options: any) {
         this.graphie = graphie;
+        // @ts-expect-error - TS2740 - modify() fills in the rest of the
+        // default state.
         this.state = {
             // Set here because this must be unique for each instance
             id: _.uniqueId("movable"),
         };
 
         // We only set DEFAULT_STATE once, here
-        // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
-        this.modify(_.extend({}, DEFAULT_STATE, options));
+        this.modify({...DEFAULT_STATE, options});
     }
-}
 
-InteractiveUtil.createGettersFor(
-    Movable,
-    _.extend({}, DEFAULT_PROPS, DEFAULT_STATE),
-);
-InteractiveUtil.addMovableHelperMethodsTo(Movable);
+    cloneState(): State {
+        return {...this.state};
+    }
 
-_.extend(Movable.prototype, {
-    cloneState: function () {
-        return _.clone(this.state);
-    },
+    protected _createDefaultState() {
+        return {
+            id: this.state.id,
+            add: [],
+            modify: [],
+            draw: [],
+            remove: [],
+            onMoveStart: [],
+            onMove: [],
+            onMoveEnd: [],
+            onClick: [],
 
-    _createDefaultState: function () {
-        return _.extend(
-            {
-                id: this.state.id,
-                add: [],
-                modify: [],
-                draw: [],
-                remove: [],
-                onMoveStart: [],
-                onMove: [],
-                onMoveEnd: [],
-                onClick: [],
+            // We only update props here, because we want things on state to
+            // be persistent, and updated appropriately in modify()
+            ...DEFAULT_PROPS,
+        };
+    }
 
-                // We only update props here, because we want things on state to
-                // be persistent, and updated appropriately in modify()
+    /**
+     * Fire an onSomething type event to all functions in listeners
+     */
+    _fireEvent(listeners, currentValue?, previousValue?) {
+        _.invoke(listeners, "call", this, currentValue, previousValue);
+    }
+
+    /**
+     * Combine the array of constraints functions
+     * Returns either an [x, y] coordinate or false
+     */
+    _applyConstraints(current, previous, extraOptions) {
+        let skipRemaining = false;
+
+        return _.reduce(
+            this.state.constraints,
+            (memo, constraint) => {
+                // A move that has been cancelled won't be propagated to later
+                // constraints calls
+                if (memo === false) {
+                    return false;
+                }
+
+                if (skipRemaining) {
+                    return memo;
+                }
+
+                const result = constraint.call(this, memo, previous, {
+                    onSkipRemaining: () => {
+                        skipRemaining = true;
+                    },
+                    ...extraOptions,
+                });
+
+                if (result === false) {
+                    // Returning false cancels the move
+                    return false;
+                }
+                if (kpoint.is(result, 2)) {
+                    // Returning a coord from constraints overrides the move
+                    return result;
+                }
+                if (result === true || result == null) {
+                    // Returning true or undefined allow the move to occur
+                    return memo;
+                }
+                // Anything else is an error
+                throw new PerseusError(
+                    "Constraint returned invalid result: " + result,
+                    Errors.Internal,
+                );
             },
-            DEFAULT_PROPS,
+            current,
+            this,
         );
-    },
+    }
+
+    /**
+     * Call all draw functions, and update our prevState for the next
+     * draw function
+     */
+    draw() {
+        const currState = this.cloneState();
+        this._fireEvent(this.state.draw, currState, this.prevState);
+        this.prevState = currState;
+    }
+
+    /**
+     * Add a listener to any event: startMove, constraints, onMove, onMoveEnd,
+     * etc. If a listener is already bound to the given eventName and id, then
+     * it is overwritten by func.
+     *
+     * eventName: the string name of the event to listen to. one of:
+     *   "onMoveStart", "onMove", "onMoveEnd", "draw", "remove"
+     *
+     * id: a string id that can be used to remove this event at a later time
+     *   note: adding multiple listeners with the same id is undefined behavior
+     *
+     * func: the function to call when the event happens, which is called
+     *   with the event's standard parameters [usually (coord, prevCoord) or
+     *   (state, prevState)]
+     */
+    listen(eventName, id, func) {
+        this._listenerMap = this._listenerMap || {};
+
+        // If there's an existing handler, replace it by using its index in
+        // `this.state[eventName]`; otherwise, add this handler to the end
+        const key = getKey(eventName, id);
+        const index = (this._listenerMap[key] =
+            this._listenerMap[key] || this.state[eventName].length);
+        this.state[eventName][index] = func;
+    }
+
+    /**
+     * Remove a previously added listener, by the id specified in the
+     * corresponding listen() call
+     *
+     * If the given id has not been registered already, this is a no-op
+     */
+    unlisten(eventName, id) {
+        this._listenerMap = this._listenerMap || {};
+
+        const key = getKey(eventName, id);
+        const index = this._listenerMap[key];
+        if (index !== undefined) {
+            // Remove handler from list of event handlers and listenerMap
+            this.state[eventName].splice(index, 1);
+            delete this._listenerMap[key];
+
+            // Re-index existing events: if they occur after `index`, decrement
+            const keys = _.keys(this._listenerMap);
+            _.each(
+                keys,
+                function (key) {
+                    if (
+                        getEventName(key) === eventName &&
+                        // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+                        this._listenerMap[key] > index
+                    ) {
+                        // @ts-expect-error - TS2683 - 'this' implicitly has type 'any' because it does not have a type annotation.
+                        this._listenerMap[key]--;
+                    }
+                },
+                this,
+            );
+        }
+    }
 
     /**
      * Resets the object to its state as if it were constructed with
@@ -106,14 +267,14 @@ _.extend(Movable.prototype, {
      *
      * Analogous to React.js's replaceProps
      */
-    modify: function (options) {
-        this.update(_.extend({}, this._createDefaultState(), options));
-    },
+    modify(options) {
+        this.update({...this._createDefaultState(), ...options});
+    }
 
     /**
      * Simulates a mouse grab event on the movable object.
      */
-    grab: function (coord) {
+    grab(coord) {
         assert(kpoint.is(coord));
         const self = this;
         const graphie = self.graphie;
@@ -151,7 +312,7 @@ _.extend(Movable.prototype, {
 
         $(document).bind("vmousemove", moveHandler);
         $(document).bind("vmouseup", upHandler);
-    },
+    }
 
     /**
      * Adjusts constructor parameters without changing previous settings
@@ -159,7 +320,7 @@ _.extend(Movable.prototype, {
      *
      * Analogous to React.js's setProps
      */
-    update: function (options) {
+    update(options) {
         const self = this;
         const graphie = self.graphie;
 
@@ -249,9 +410,9 @@ _.extend(Movable.prototype, {
 
         // Trigger a modify event
         self._fireEvent(state.modify, self.cloneState(), self.prevState);
-    },
+    }
 
-    remove: function () {
+    remove() {
         this.state.added = false;
         this._fireEvent(this.state.remove);
         if (this.state.mouseTarget) {
@@ -259,21 +420,27 @@ _.extend(Movable.prototype, {
             this.state.mouseTarget.remove();
             this.state.mouseTarget = null;
         }
-    },
+    }
 
     // Change z-order to back
-    toBack: function () {
+    toBack() {
         if (this.state.mouseTarget) {
             this.state.mouseTarget.toBack();
         }
-    },
+    }
 
     // Change z-order to front
-    toFront: function () {
+    toFront() {
         if (this.state.mouseTarget) {
             this.state.mouseTarget.toFront();
         }
-    },
-});
+    }
+}
+
+InteractiveUtil.createGettersFor(
+    Movable,
+    _.extend({}, DEFAULT_PROPS, DEFAULT_STATE),
+);
+// InteractiveUtil.addMovableHelperMethodsTo(Movable);
 
 export default Movable;
